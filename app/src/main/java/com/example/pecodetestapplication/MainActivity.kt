d3c83e302f476da77e7243820650cec9705ff2ca
Bronnick <114607031+Bronnick@users.noreply.github.com>
2024-05-16 17:03:05 +0300

    created basic layout

--- app/src/main/java/com/example/pecodetestapplication/MainActivity.kt
+++ app/src/main/java/com/example/pecodetestapplication/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.pecodetestapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pecodetestapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding?.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding?.buttonPlus?.setBackgroundColor(R.color.white)
+
+        binding?.buttonPlus?.setOnClickListener {
+            Toast.makeText(this, "Boo!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
